24c6883b47253faeb6dc38c658b6c6af515fe5a2
SeydouDAOU <daouseydou@yahoo.com>
2025-03-17 14:29:57 +0100

    link avatar

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://Dalab21.github.io",
+  base: "/CvDaouDev",
+  trailingSlash: "always",
   integrations: [tailwind()],
 });
